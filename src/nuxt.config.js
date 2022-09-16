f6729a4360f6958d10d9f5081f79a546d29c5aff
Vawlpe <vawlpe@gmail.com>
2022-09-16 18:01:25 +0300

    Temporarily remove Nightwatch.js e2e testing framework

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 
   // Router
   router: {
-    base: '/'
+    base: '/',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -42,16 +42,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
